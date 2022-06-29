7a98704e540a7163e42c3a3454a6691c0120e167
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-06-29 16:40:13 +0300

    bug found

--- app/src/main/java/com/rorono/a22recycler/utils/Rounding.kt
+++ app/src/main/java/com/rorono/a22recycler/utils/Rounding.kt
@@ -3,6 +3,6 @@ package com.rorono.a22recycler.utils
 import kotlin.math.roundToInt
 
 
-object Rounding {
+object Rounding { //исправить
     fun getTwoNumbersAfterDecimalPoint(value: Double): Double = (value * 100).roundToInt() / 100.0
 }
