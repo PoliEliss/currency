1d9b8f1ab952f2e259b6fcba1aac433a979d14f7
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-06-29 18:11:11 +0300

    add menu

--- app/src/main/java/com/rorono/a22recycler/presentation/MainActivity.kt
+++ app/src/main/java/com/rorono/a22recycler/presentation/MainActivity.kt
@@ -3,6 +3,9 @@ package com.rorono.a22recycler.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.rorono.a22recycler.CurrencyViewModel
 import com.rorono.a22recycler.MainViewModelFactory
 import com.rorono.a22recycler.R
@@ -26,5 +29,11 @@ class MainActivity : AppCompatActivity() {
         val dataBaseDao = dataBase.currencyDao()
         val viewModelFactory = MainViewModelFactory(repository, dataBase = dataBaseDao)
         viewModel = ViewModelProvider(this, viewModelFactory)[CurrencyViewModel::class.java]
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigationView.setupWithNavController(navController = navController)
     }
 }
