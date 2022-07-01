0a942b89a4beacabc0569ab901122eba874eec17
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-01 20:41:39 +0300

    change Repository

--- app/src/main/java/com/rorono/a22recycler/repository/Repository.kt
+++ app/src/main/java/com/rorono/a22recycler/repository/Repository.kt
@@ -1,5 +1,6 @@
 package com.rorono.a22recycler.repository
 
+import android.util.Log
 import com.rorono.a22recycler.Result
 import com.rorono.a22recycler.network.RetrofitInstance
 import kotlinx.coroutines.Dispatchers
@@ -7,16 +8,20 @@ import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class Repository(private val retrofit: RetrofitInstance) {
-    suspend fun getCurrency(data: String): Result = withContext(Dispatchers.IO) {
-        try {
-            val response = retrofit.api.getCurrency(data = data)
-            if (response.isSuccessful) {
-                return@withContext Result.Success(response.body()!!.currencies)
-            } else {
-                return@withContext Result.Error("Не удалось отобразить данные за этот день \n Пожалуйста, выберите другую дату")
+    suspend fun getCurrency(data: String): Result = withContext(Dispatchers.IO)  {
+      //  if (!netManager.isOnline()) return@withContext Result.NoInternetConnecting
+            try {
+                val response = retrofit.api.getCurrency(data = data)
+                if (response.isSuccessful) {
+                    Log.d("TEST","response.isSuccessful)")
+                    return@withContext Result.Success(response.body()!!.currencies)
+                } else {
+                    Log.d("TEST","esult.Error1)")
+                    return@withContext Result.Error("Не удалось отобразить данные за этот день \n Пожалуйста, выберите другую дату")
+                }
+            } catch (e: Exception) {
+                Log.d("TEST","e exception ${e}")
+                return@withContext Result.Error("Отсутствует интернет подключение")
             }
-        } catch (e: Exception) {
-            return@withContext Result.Error("Отсутствует интернет подключение")
-        }
     }
 }
