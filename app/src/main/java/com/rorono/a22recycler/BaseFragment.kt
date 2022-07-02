bc488913e572b8182aa3d224eaf9b6be79e39682
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-02 18:52:58 +0300

    add BaseFragment and BaseViewBindingFragment

---
+++ app/src/main/java/com/rorono/a22recycler/BaseFragment.kt
@@ -0,0 +1,10 @@
+package com.rorono.a22recycler
+
+import androidx.fragment.app.Fragment
+import com.rorono.a22recycler.presentation.MainActivity
+
+open class BaseFragment : Fragment() {
+    override fun onResume() {
+        super.onResume()
+    }
+}
---
+++ app/src/main/java/com/rorono/a22recycler/BaseViewBindingFragment.kt
@@ -0,0 +1,31 @@
+package com.rorono.a22recycler
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseViewBindingFragment<VB:ViewBinding>(private val inflate:(LayoutInflater,ViewGroup?,Boolean)->VB):BaseFragment() {
+    private var _binding: VB? = null
+    val binding
+        get() = _binding
+            ?: throw IllegalStateException("Trying to access binding in ${this.lifecycle.currentState} state")
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
