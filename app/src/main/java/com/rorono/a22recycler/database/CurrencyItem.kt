739a0d2aef88bad7d42b26e059a5945122470e32
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-03 10:15:43 +0300

    removed comment

--- app/src/main/java/com/rorono/a22recycler/database/CurrencyItem.kt
+++ app/src/main/java/com/rorono/a22recycler/database/CurrencyItem.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "currency")
 data class CurrencyItem(
-    @PrimaryKey val fullName: String,//id
+    @PrimaryKey val fullName: String,
     @ColumnInfo(name = "char_code")
     val charCode: String,
     @ColumnInfo(name = "value")
