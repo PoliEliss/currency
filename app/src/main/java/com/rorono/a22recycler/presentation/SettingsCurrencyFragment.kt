4abf24689d71508ecdbe3b659afe16e0f7d08841
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-11 18:21:06 +0300

    add Settings fun saveTheme

--- app/src/main/java/com/rorono/a22recycler/presentation/SettingsCurrencyFragment.kt
+++ app/src/main/java/com/rorono/a22recycler/presentation/SettingsCurrencyFragment.kt
@@ -8,17 +8,23 @@ import android.view.ViewGroup
 import com.rorono.a22recycler.BaseViewBindingFragment
 import com.rorono.a22recycler.R
 import com.rorono.a22recycler.databinding.FragmentSettingsCurrencyBinding
+import com.rorono.a22recycler.settings.Settings
 
 
-class SettingsCurrencyFragment : BaseViewBindingFragment<FragmentSettingsCurrencyBinding>(FragmentSettingsCurrencyBinding::inflate) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+class SettingsCurrencyFragment :
+    BaseViewBindingFragment<FragmentSettingsCurrencyBinding>(FragmentSettingsCurrencyBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonChooseDayTheme.setOnClickListener {
+            Settings.saveTheme(requireContext(), 1)
+            requireActivity().recreate()
+        }
+        binding.buttonChooseNightTheme.setOnClickListener {
+            Settings.saveTheme(requireContext(), 2)
+            requireActivity().recreate()
+        }
     }
 
 }
