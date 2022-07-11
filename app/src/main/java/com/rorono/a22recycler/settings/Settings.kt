607066840e9149438611959374a306b021f2fb7a
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-11 18:09:39 +0300

    add object Settings

---
+++ app/src/main/java/com/rorono/a22recycler/settings/Settings.kt
@@ -0,0 +1,38 @@
+package com.rorono.a22recycler.settings
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object Settings {
+
+    private const val THEME = "chosen_theme"
+    private const val LANGUAGE = "chosen_language"
+    private const val KEY_THEME = "key_theme"
+    private const val KEY_LANGUAGE = "key_language"
+
+
+    fun saveTheme(context: Context, theme: Int) {
+        val sharedPreferences = context.getSharedPreferences(THEME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_THEME, theme)
+        editor.apply()
+    }
+
+    fun saveLanguage(context: Context, language: Int) {
+        val sharedPreferences = context.getSharedPreferences(LANGUAGE, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_LANGUAGE, language)
+        editor.apply()
+
+    }
+
+    fun loadTheme(context: Context): Int {
+        val sharedPreferences = context.getSharedPreferences(THEME, Context.MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_THEME, 1)
+    }
+
+    fun loadLanguage(context: Context): Int {
+        val sharedPreferences = context.getSharedPreferences(LANGUAGE, Context.MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_LANGUAGE, 0)
+    }
+}
