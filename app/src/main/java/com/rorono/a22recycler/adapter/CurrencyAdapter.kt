e38b2353c49ffef7b8c734086422304c82eba273
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-12 17:22:43 +0300

    change CurrencyAdapter on ListAdapter

--- app/src/main/java/com/rorono/a22recycler/adapter/CurrencyAdapter.kt
+++ app/src/main/java/com/rorono/a22recycler/adapter/CurrencyAdapter.kt
@@ -3,23 +3,56 @@ package com.rorono.a22recycler.adapter
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.rorono.a22recycler.R
 import com.rorono.a22recycler.databinding.CurrencyItemBinding
 import com.rorono.a22recycler.models.Currency
 import com.rorono.a22recycler.utils.Rounding
 
-class CurrencyAdapter(private val onItemClickListener: OnItemClickListener) : RecyclerView.Adapter<CurrencyAdapter.CurrencyHolder>() {
+class CurrencyAdapter(private val onItemClickListener: OnItemClickListener) :
+    ListAdapter<Currency, CurrencyAdapter.CurrencyHolder>(
+        DiffUtil()
+    ) {
+    class DiffUtil : androidx.recyclerview.widget.DiffUtil.ItemCallback<Currency>() {
+        override fun areItemsTheSame(oldItem: Currency, newItem: Currency): Boolean {
+            return oldItem == newItem
+        }
 
-    private var currencyList = mutableListOf<Currency>()
+        override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean {
+            return when {
+                oldItem.fullName != newItem.fullName -> {
+                    return false
+                }
+                oldItem.isFavorite != newItem.isFavorite -> {
+                    return false
+                }
+                oldItem.charCode != newItem.charCode -> {
+                    return false
+                }
+                oldItem.value != newItem.value -> {
+                    return false
+                }
+                else -> true
+            }
+        }
+    }
+
 
     inner class CurrencyHolder(binding: CurrencyItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         private val textViewNameRate = binding.textViewNameRate
         private val textViewExchangeRate = binding.textViewExchangeRate
+        private val ivFavorite = binding.ivFavorite
 
         fun bind(currency: Currency) {
+            if (currency.isFavorite == 1) {
+                ivFavorite.setImageResource(R.drawable.ic_favorite)
+            } else {
+                ivFavorite.setImageResource(R.drawable.ic_favorite_border)
+            }
             itemView.setOnClickListener {
-                onItemClickListener.onItemClick(currency=currency)
+                onItemClickListener.onItemClick(currency = currency)
             }
             textViewNameRate.text = currency.charCode
 
@@ -36,19 +69,7 @@ class CurrencyAdapter(private val onItemClickListener: OnItemClickListener) : Re
     }
 
     override fun onBindViewHolder(holder: CurrencyHolder, position: Int) {
-        holder.bind(currencyList[position])
+        holder.bind(getItem(position))
     }
 
-    override fun getItemCount(): Int {
-        return currencyList.size
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    fun setItems(items: List<Currency>) {
-        currencyList.apply {
-            clear()
-            addAll(items)
-        }
-        notifyDataSetChanged()
-    }
 }
