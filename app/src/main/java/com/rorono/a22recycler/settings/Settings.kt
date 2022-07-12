c81e51ca9212b63803ea2643bf477c53f92b85d7
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-12 17:28:24 +0300

    fun saveOrientation and fun loadOrientation added

--- app/src/main/java/com/rorono/a22recycler/settings/Settings.kt
+++ app/src/main/java/com/rorono/a22recycler/settings/Settings.kt
@@ -7,9 +7,10 @@ object Settings {
 
     private const val THEME = "chosen_theme"
     private const val LANGUAGE = "chosen_language"
+    private const val ORIENTATION = "chosen_orientation"
     private const val KEY_THEME = "key_theme"
     private const val KEY_LANGUAGE = "key_language"
-
+    private const val KEY_ORIENTATION = "key_orientation"
 
     fun saveTheme(context: Context, theme: Int) {
         val sharedPreferences = context.getSharedPreferences(THEME, Context.MODE_PRIVATE)
@@ -26,6 +27,13 @@ object Settings {
 
     }
 
+    fun saveOrientation(context: Context, orientation: Int) {
+        val sharedPreferences = context.getSharedPreferences(ORIENTATION, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_ORIENTATION, orientation)
+        editor.apply()
+    }
+
     fun loadTheme(context: Context): Int {
         val sharedPreferences = context.getSharedPreferences(THEME, Context.MODE_PRIVATE)
         return sharedPreferences.getInt(KEY_THEME, 1)
@@ -34,5 +42,10 @@ object Settings {
     fun loadLanguage(context: Context): Int {
         val sharedPreferences = context.getSharedPreferences(LANGUAGE, Context.MODE_PRIVATE)
         return sharedPreferences.getInt(KEY_LANGUAGE, 1)
+    }
+
+    fun loadOrientation(context: Context): Int {
+        val sharedPreferences = context.getSharedPreferences(ORIENTATION, Context.MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_ORIENTATION, 1)
     }
 }
