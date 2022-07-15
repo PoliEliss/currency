58fd630a332de5e10c4e4e76aff0da2473c0604b
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-15 13:34:26 +0300

    add onItemClickDeleteFavoriteCurrency

--- app/src/main/java/com/rorono/a22recycler/adapter/OnItemClickChosenCurrency.kt
+++ app/src/main/java/com/rorono/a22recycler/adapter/OnItemClickChosenCurrency.kt
@@ -5,4 +5,6 @@ import com.rorono.a22recycler.models.Currency
 interface OnItemClickChosenCurrency {
 
     fun onItemClick(currency: Currency)
+
+    fun onItemClickDeleteFavoriteCurrency(currency: Currency)
 }
