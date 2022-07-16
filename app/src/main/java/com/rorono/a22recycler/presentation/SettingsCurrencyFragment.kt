146b0659cfa42ec7e838b9c5d0f71c7519aba547
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-16 09:30:06 +0300

    change themes

--- app/src/main/java/com/rorono/a22recycler/presentation/SettingsCurrencyFragment.kt
+++ app/src/main/java/com/rorono/a22recycler/presentation/SettingsCurrencyFragment.kt
@@ -1,6 +1,9 @@
 package com.rorono.a22recycler.presentation
 
+import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.rorono.a22recycler.BaseViewBindingFragment
 import com.rorono.a22recycler.R
@@ -23,7 +27,15 @@ class SettingsCurrencyFragment :
 
         when (Settings.loadTheme(requireContext())) {
             1 -> binding.radioButtonLightTheme.isChecked = true
-            2 -> binding.radioButtonDarkTheme.isChecked = true
+            2 -> {
+                binding.radioButtonDarkTheme.isChecked = true
+                binding.radioButtonDarkTheme.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                binding.radioButtonChooseEN.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                binding.radioButtonChooseRU.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                binding.radioButtonChooseTile.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                binding.radioButtonChooseLine.setBackgroundResource(R.drawable.radio_selector_black_theme)
+
+            }
         }
         when(Settings.loadLanguage(requireContext())){
             1 -> binding.radioButtonChooseRU.isChecked = true
