16290b82bccbdb3f81e10f7aff9fa3553d288365
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-17 12:33:30 +0300

    rewritten on  Dagger

--- app/src/main/java/com/rorono/a22recycler/presentation/SavedCurrencyFragment.kt
+++ app/src/main/java/com/rorono/a22recycler/presentation/SavedCurrencyFragment.kt
@@ -1,49 +1,73 @@
 package com.rorono.a22recycler.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.rorono.a22recycler.BaseViewBindingFragment
+import com.rorono.a22recycler.MainViewModelFactory
+import com.rorono.a22recycler.MyApplication
 import com.rorono.a22recycler.adapter.ChosenCurrencyAdapter
 import com.rorono.a22recycler.adapter.CurrenciesSaveAdapter
 import com.rorono.a22recycler.adapter.OnItemClickChosenCurrency
+import com.rorono.a22recycler.database.CurrencyDao
+import com.rorono.a22recycler.database.CurrencyDataBase
 import com.rorono.a22recycler.database.SaveCurrencyItem
 import com.rorono.a22recycler.databinding.FragmentSavedCurrencyBinding
 import com.rorono.a22recycler.models.Currency
+import com.rorono.a22recycler.network.RetrofitInstance
+import com.rorono.a22recycler.repository.Repository
 import com.rorono.a22recycler.settings.Settings
 import com.rorono.a22recycler.viewmodel.CurrencyViewModel
+import java.io.File
 import java.util.*
+import javax.inject.Inject
 
 
 class SavedCurrencyFragment :
     BaseViewBindingFragment<FragmentSavedCurrencyBinding>(FragmentSavedCurrencyBinding::inflate) {
+
+    @Inject
+    lateinit var factory: MainViewModelFactory
+    lateinit var viewModel: CurrencyViewModel
+
     private val adapter = CurrenciesSaveAdapter()
     private var behavior: BottomSheetBehavior<*>? = null
-    private val adapterChosenCurrency = ChosenCurrencyAdapter(object : OnItemClickChosenCurrency {
-        override fun onItemClick(currency: Currency) {
-            val action =
-                SavedCurrencyFragmentDirections.actionSavedCurrencyFragmentToCurrencyTransferFragment(
-                    currency
-                )
-            findNavController().navigate(action)
-        }
-        override fun onItemClickDeleteFavoriteCurrency(currency: Currency) {
-            viewModel.deleteSaveCurrency(currency)
-            viewModel.getCurrencyDao()
-            viewModel.getSaveCurrencyDao()
-        }
-    })
-    private val viewModel by activityViewModels<CurrencyViewModel>()
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    private val adapterChosenCurrency = ChosenCurrencyAdapter()
 
+
+
+    override fun onAttach(context: Context) {
+        (context.applicationContext as MyApplication).appComponent.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        adapterChosenCurrency.setOnListener(object :OnItemClickChosenCurrency{
+            override fun onItemClick(currency: Currency, position: Int) {
+                val action =
+                    SavedCurrencyFragmentDirections.actionSavedCurrencyFragmentToCurrencyTransferFragment(
+                        currency
+                    )
+                findNavController().navigate(action)
+            }
+            override fun onItemClickDeleteFavoriteCurrency(currency: Currency) {
+                viewModel.deleteSaveCurrency(currency)
+                viewModel.getCurrencyDao()
+                viewModel.getSaveCurrencyDao()
+            }
+
+        })
+        viewModel = ViewModelProvider(this, factory)[CurrencyViewModel::class.java]
         behavior = BottomSheetBehavior.from(binding.include.bottomSheet)
         behavior!!.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
@@ -99,7 +123,7 @@ class SavedCurrencyFragment :
                         listCurrency.add(i)
                     }
                     adapter.setData(listCurrency)
-                    adapter.notifyDataSetChanged()
+                    //adapter.notifyItemInserted()
                 }
                 viewModel.setSaveCurrencyDao(listFavoriteCurrency)
                 viewModel.getSaveCurrencyDao()
@@ -148,25 +172,23 @@ class SavedCurrencyFragment :
             }
 
             override fun onSwiped(viewHolder: ViewHolder, direction: Int) {
-               /* val position = viewHolder.bindingAdapterPosition
-                val currency =
-                    adapterChosenCurrency.chosenCurrencyList.removeAt(position) //Это уже работать не будет
-                adapterChosenCurrency.notifyItemRemoved(position)
-                Log.d("TEST", "currency!!! ${currency}")
-                val saveCurrency = SaveCurrencyItem(
-                    fullName = currency.fullName,
-                    charCode = currency.charCode,
-                    value = currency.value
-                )
-                Log.d("TEST", "saveCurrency ${saveCurrency}")
-                //viewModel.deleteSaveCurrency(saveCurrency) // вот только он не удаляет, во ViewModel я попадаю
-                // одной валюты из бд но опять же тут по позиции идет удаление а у меня по id
-                // должно быть*/
+                /* val position = viewHolder.bindingAdapterPosition
+                 val currency =
+                     adapterChosenCurrency.chosenCurrencyList.removeAt(position) //Это уже работать не будет
+                 adapterChosenCurrency.notifyItemRemoved(position)
+                 Log.d("TEST", "currency!!! ${currency}")
+                 val saveCurrency = SaveCurrencyItem(
+                     fullName = currency.fullName,
+                     charCode = currency.charCode,
+                     value = currency.value
+                 )
+                 Log.d("TEST", "saveCurrency ${saveCurrency}")
+                 //viewModel.deleteSaveCurrency(saveCurrency) // вот только он не удаляет, во ViewModel я попадаю
+                 // одной валюты из бд но опять же тут по позиции идет удаление а у меня по id
+                 // должно быть*/
 
             }
         })
-
-
 
 
     fun changAdapter(change: Int) {
--- app/src/main/java/com/rorono/a22recycler/repository/Repository.kt
+++ app/src/main/java/com/rorono/a22recycler/repository/Repository.kt
@@ -2,24 +2,23 @@ package com.rorono.a22recycler.repository
 
 import android.util.Log
 import com.rorono.a22recycler.Result
+import com.rorono.a22recycler.network.ApiService
 import com.rorono.a22recycler.network.RetrofitInstance
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.lang.Exception
+import javax.inject.Inject
 
-class Repository(private val retrofit: RetrofitInstance) {
+class Repository @Inject constructor(private val apiService: ApiService) {
     suspend fun getCurrency(data: String): Result = withContext(Dispatchers.IO)  {
             try {
-                val response = retrofit.api.getCurrency(data = data)
+                val response = apiService.getCurrency(data = data)
                 if (response.isSuccessful) {
-                    Log.d("TEST","response.isSuccessful)")
                     return@withContext Result.Success(response.body()!!.currencies)
                 } else {
-                    Log.d("TEST","esult.Error1)")
                     return@withContext Result.Error("Не удалось отобразить данные за этот день \n Пожалуйста, выберите другую дату")
                 }
             } catch (e: Exception) {
-                Log.d("TEST","e exception ${e}")
                 return@withContext Result.Error("Отсутствует интернет подключение")
             }
     }
