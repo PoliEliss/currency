512a44420133527778c191da01349d2c0e2e0f1d
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-24 12:00:05 +0300

    delete behavior, change delete favorite currency

--- app/src/main/java/com/rorono/a22recycler/presentation/SavedCurrencyFragment.kt
+++ app/src/main/java/com/rorono/a22recycler/presentation/SavedCurrencyFragment.kt
@@ -38,8 +38,10 @@ class SavedCurrencyFragment :
     @Inject
     lateinit var factory: MainViewModelFactory
     lateinit var viewModel: CurrencyViewModel
-    private val adapter = CurrenciesSaveAdapter()
-    private var behavior: BottomSheetBehavior<*>? = null
+
+    //private val adapter = CurrenciesSaveAdapter()
+    // private var behavior: BottomSheetBehavior<*>? = null
+
     private val adapterChosenCurrency = ChosenCurrencyAdapter()
 
 
@@ -60,113 +62,40 @@ class SavedCurrencyFragment :
                 findNavController().navigate(action)
             }
 
-            override fun onItemClickDeleteFavoriteCurrency(currency: Currency,position:Int) {
+            override fun onItemClickDeleteFavoriteCurrency(currency: Currency, position: Int) {
+                val listFavorite = mutableListOf<Currency>()
+                viewModel.saveCurrencyDatabase.value?.let { listFavorite.addAll(it) }
 
-                val listSaveCurrency = mutableListOf<Currency>()
-                for (i in viewModel.saveCurrencyDatabase.value!!){
-                    listSaveCurrency.add(i)
-                }
-                val listCurrency = mutableListOf<Currency>()
-                for (i in viewModel.currencyDatabase.value!!) {
-                    listCurrency.add(i)
-                    if (i.fullName == currency.fullName) {
-                        listCurrency.remove(i)
-                        i.isFavorite = 0
-                        listCurrency.add(i)
-                    }
-                    adapter.submitList(listCurrency)
-                    adapter.notifyItemChanged(position)
-                    adapter.notifyDataSetChanged()
-                }
                 viewModel.deleteSaveCurrency(currency)
-                listSaveCurrency.remove(currency)
+                listFavorite.remove(currency)
+                Log.d("TEST15","currency ${viewModel.saveCurrencyDatabase.value}")
+                Log.d("TEST15","ListFavprite ${listFavorite}")
 
-                adapterChosenCurrency.setData(listSaveCurrency)
+                adapterChosenCurrency.setData(listFavorite)
                 adapterChosenCurrency.notifyItemRemoved(position)
                 viewModel.getSaveCurrencyDao()
+
             }
         })
         viewModel = ViewModelProvider(this, factory)[CurrencyViewModel::class.java]
-        behavior = BottomSheetBehavior.from(binding.include.bottomSheet)
-        behavior!!.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
-            override fun onStateChanged(bottomSheet: View, newState: Int) {
-                binding.fab.visibility = when (newState) {
-                    BottomSheetBehavior.STATE_COLLAPSED -> View.VISIBLE
-                    else -> View.GONE
-                }
-            }
 
-            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
-
-        })
-        binding.fab.setOnClickListener {
-            behavior!!.state = BottomSheetBehavior.STATE_EXPANDED
-        }
         when (Settings.loadOrientation(requireContext())) {
             1 -> changAdapter(1)
             2 -> changAdapter(2)
         }
-        binding.include.recyclerViewSaveCurrency.adapter = adapter
 
-        viewModel.getCurrencyDao()
+
+
+
         viewModel.getSaveCurrencyDao()
 
-        viewModel.currencyDatabase.observe(viewLifecycleOwner) { listCurrency ->
-            viewModel.saveCurrencyDatabase.observe(viewLifecycleOwner) { listDataBase ->
-                for (i in listCurrency) {
-                    for (g in listDataBase) {
-                        if (g.fullName == i.fullName) {
-                            i.isFavorite = 1
-                        }
-                    }
-                    adapter.submitList(listCurrency)
-                    adapterChosenCurrency.setData(listDataBase)
+               //
+        viewModel.saveCurrencyDatabase.observe(viewLifecycleOwner) {
+            Log.d("TEST15","addDataBaseCurrency ${it}")
+            Log.d("TEST15","viewModel ${viewModel.saveCurrencyDatabase.value}")
+            adapterChosenCurrency.setData(it)
 
-                }
-            }
-            adapter.submitList(listCurrency)
         }
-
-        adapter.setFavoriteListener(object : OnItemClickListener {
-            override fun onItemClick(currency: Currency,position:Int) {
-                val listFavoriteCurrency = mutableListOf<Currency>()
-                listFavoriteCurrency.add(currency)
-                if (currency.isFavorite == 0) {
-                    Toast.makeText(requireContext(), "ADD", Toast.LENGTH_LONG).show()
-                    val listCurrency = mutableListOf<Currency>()
-                    for (i in viewModel.currencyDatabase.value!!) {
-                        listCurrency.add(i)
-                        if (i.fullName == currency.fullName) {
-                            listCurrency.remove(i)
-                            i.isFavorite = 1
-                            listCurrency.add(i)
-                        }
-                        adapter.submitList(listCurrency)
-                        adapter.notifyItemChanged(position)
-                        adapterChosenCurrency.setData(listFavoriteCurrency)
-                    }
-                    viewModel.setSaveCurrencyDao(listFavoriteCurrency)
-                    viewModel.getSaveCurrencyDao()
-                } else {
-                    val listCurrency = mutableListOf<Currency>()
-                    for (i in viewModel.currencyDatabase.value!!) {
-                        listCurrency.add(i)
-                        if (i.fullName == currency.fullName) {
-                            listCurrency.remove(i)
-                            i.isFavorite = 0
-                            listCurrency.add(i)
-                        }
-                        adapter.submitList(listCurrency)
-                        adapter.notifyItemChanged(position)
-                    }
-                    viewModel.deleteSaveCurrency(currency)
-                    listFavoriteCurrency.remove(currency)
-                    adapterChosenCurrency.setData(listFavoriteCurrency)
-                    adapterChosenCurrency.notifyItemChanged(position)
-                    viewModel.getSaveCurrencyDao()
-                }
-            }
-        })
 
         val mIth = ItemTouchHelper(
             object : ItemTouchHelper.SimpleCallback(
@@ -197,7 +126,8 @@ class SavedCurrencyFragment :
     }
 
     fun changAdapter(change: Int) {
-        (binding.recyclerViewChosenCurrency.itemAnimator as SimpleItemAnimator).supportsChangeAnimations=false
+        (binding.recyclerViewChosenCurrency.itemAnimator as SimpleItemAnimator).supportsChangeAnimations =
+            false
 
         if (change == 1) {
             binding.apply {
