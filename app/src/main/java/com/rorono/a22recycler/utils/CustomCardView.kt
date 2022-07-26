4eff912dad01448d62d04c93a737b82045534658
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-26 21:09:21 +0300

    CustomCardView added

---
+++ app/src/main/java/com/rorono/a22recycler/utils/CustomCardView.kt
@@ -0,0 +1,17 @@
+package com.rorono.a22recycler.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.cardview.widget.CardView
+
+class CustomCardView(context: Context, attrs:AttributeSet):CardView(context,attrs) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec)
+
+    }
+
+
+    //todo надо указать что атрибут по x равен атрибуту по Y
+    //
+}
