be8c35b2df791c993778c0251eed51a64465449d
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-26 13:29:18 +0300

    getItemViewType added

--- app/src/main/java/com/rorono/a22recycler/adapter/ChosenCurrencyAdapter.kt
+++ app/src/main/java/com/rorono/a22recycler/adapter/ChosenCurrencyAdapter.kt
@@ -3,30 +3,42 @@ package com.rorono.a22recycler.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.rorono.a22recycler.R
 import com.rorono.a22recycler.databinding.ChosenCurrencyItemBinding
 import com.rorono.a22recycler.databinding.CurrencyItemBinding
+import com.rorono.a22recycler.databinding.ListChosenCurrencyItemBinding
 import com.rorono.a22recycler.models.Currency
 import com.rorono.a22recycler.utils.Rounding
 
 
-class ChosenCurrencyAdapter() :
-    RecyclerView.Adapter<ChosenCurrencyAdapter.ChosenCurrencyHolder>() {
+private const val LIST_TYPE = 0
+private const val GRID_TYPE = 1
 
-   lateinit var onItemClickChosenCurrency: OnItemClickChosenCurrency
+class ChosenCurrencyAdapter() :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    lateinit var onItemClickChosenCurrency: OnItemClickChosenCurrency
 
     fun setOnListener(listener: OnItemClickChosenCurrency) {
         onItemClickChosenCurrency = listener
     }
+
     var oldList = emptyList<Currency>()
 
-    inner class ChosenCurrencyHolder(binding: CurrencyItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        private val tvNameRate = binding.textViewNameRate
-        private val tvExchangeRate = binding.textViewExchangeRate
 
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(currency: Currency) {
+           val tvNameRate = itemView.findViewById<TextView>(R.id.textViewNameRate)
+            tvNameRate.text = currency.charCode
+            val tvExchangeRate = itemView.findViewById<TextView>(R.id.textViewExchangeRate)
+            (Rounding.getTwoNumbersAfterDecimalPoint(currency.value).toString() + "₽").also {
+                tvExchangeRate.text = it
+            }
+            val fullName = itemView.findViewById<TextView>(R.id.tvFullName)
+            fullName.text = currency.fullName
             itemView.setOnClickListener {
                 onItemClickChosenCurrency.onItemClick(currency = currency, layoutPosition)
             }
@@ -37,25 +49,63 @@ class ChosenCurrencyAdapter() :
                 )
                 true
             }
+        }
+    }
+
+    inner class ChosenCurrencyHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        fun bind(currency: Currency) {
+           val tvNameRate = itemView.findViewById<TextView>(R.id.textViewNameRate)
             tvNameRate.text = currency.charCode
+            val tvExchangeRate = itemView.findViewById<TextView>(R.id.textViewExchangeRate)
             (Rounding.getTwoNumbersAfterDecimalPoint(currency.value).toString() + "₽").also {
-                tvExchangeRate.text = it
+                tvExchangeRate.setText(it)
+            }
+            itemView.setOnClickListener {
+                onItemClickChosenCurrency.onItemClick(currency = currency, layoutPosition)
+            }
+            itemView.setOnLongClickListener {
+                onItemClickChosenCurrency.onItemClickDeleteFavoriteCurrency(
+                    currency = currency,
+                    position = layoutPosition
+                )
+                true
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChosenCurrencyHolder {
-        val binding =
-           CurrencyItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ChosenCurrencyHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == LIST_TYPE) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_chosen_currency_item, parent, false)
+            return ListViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.chosen_currency_item, parent, false)
+            return ChosenCurrencyHolder(view)
+        }
     }
 
-    override fun onBindViewHolder(holder: ChosenCurrencyHolder, position: Int) {
-        holder.bind(oldList[position])
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (getItemViewType(position) == LIST_TYPE) {
+            (holder as ListViewHolder).bind(oldList[position])
+        } else {
+            (holder as ChosenCurrencyHolder).bind(oldList[position])
+        }
+
     }
 
     override fun getItemCount(): Int {
         return oldList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (oldList[position].isFavorite == 0) {
+            LIST_TYPE
+        } else {
+            GRID_TYPE
+        }
     }
 
     fun setData(newList: List<Currency>) {
