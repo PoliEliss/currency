e111d1d1b11be60cb85fa7629f4a70e34f41d8f1
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-30 15:31:00 +0300

    with(binding) added

--- app/src/main/java/com/rorono/a22recycler/presentation/SettingsCurrencyFragment.kt
+++ app/src/main/java/com/rorono/a22recycler/presentation/SettingsCurrencyFragment.kt
@@ -1,17 +1,7 @@
 package com.rorono.a22recycler.presentation
 
-import android.content.Context
-import android.content.Intent
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.RadioButton
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.rorono.a22recycler.BaseViewBindingFragment
 import com.rorono.a22recycler.R
@@ -28,13 +18,14 @@ class SettingsCurrencyFragment :
         when (Settings.loadTheme(requireContext())) {
             1 -> binding.radioButtonLightTheme.isChecked = true
             2 -> {
-                binding.radioButtonDarkTheme.isChecked = true
-                binding.radioButtonDarkTheme.setBackgroundResource(R.drawable.radio_selector_black_theme)
-                binding.radioButtonChooseEN.setBackgroundResource(R.drawable.radio_selector_black_theme)
-                binding.radioButtonChooseRU.setBackgroundResource(R.drawable.radio_selector_black_theme)
-                binding.radioButtonChooseTile.setBackgroundResource(R.drawable.radio_selector_black_theme)
-                binding.radioButtonChooseLine.setBackgroundResource(R.drawable.radio_selector_black_theme)
-
+                with(binding) {
+                    radioButtonDarkTheme.isChecked = true
+                    radioButtonDarkTheme.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                    radioButtonChooseEN.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                    radioButtonChooseRU.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                    radioButtonChooseTile.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                    radioButtonChooseLine.setBackgroundResource(R.drawable.radio_selector_black_theme)
+                }
             }
         }
         when (Settings.loadLanguage(requireContext())) {
