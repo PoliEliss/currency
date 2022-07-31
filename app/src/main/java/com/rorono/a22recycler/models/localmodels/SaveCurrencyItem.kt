4ebf9dce06922afe0f67b147602a1e7aed2e2c78
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-07-31 15:54:50 +0300

    change package

---
+++ app/src/main/java/com/rorono/a22recycler/models/localmodels/SaveCurrencyItem.kt
@@ -0,0 +1,17 @@
+package com.rorono.a22recycler.models.localmodels
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "save_currency")
+data class SaveCurrencyItem(
+    @PrimaryKey
+    val fullName: String,
+    @ColumnInfo(name = "char_code")
+    val charCode: String,
+    @ColumnInfo(name = "value")
+    val value: Double,
+    val favorite: Int = 0
+
+)
