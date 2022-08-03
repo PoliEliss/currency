a4a8a33213fe3214ba1324082a4ce0f571ad8243
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-08-03 10:16:26 +0300

    delete comment

--- app/src/main/java/com/rorono/a22recycler/di/AppModule.kt
+++ app/src/main/java/com/rorono/a22recycler/di/AppModule.kt
@@ -2,7 +2,7 @@ package com.rorono.a22recycler.di
 
 import dagger.Module
 
-@Module(includes = [AppModuleDatabase::class,NetworkModule::class,AppModuleViewModule::class]) //factoVMModule
+@Module(includes = [AppModuleDatabase::class,NetworkModule::class,AppModuleViewModule::class])
 class AppModule {
 
 
