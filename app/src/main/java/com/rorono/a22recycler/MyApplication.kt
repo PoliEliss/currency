d732f5e70fa2d8fcc28091eebf82588eb4fbf8c1
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-09-04 10:45:30 +0300

    fun applicationContext added

--- app/src/main/java/com/rorono/a22recycler/MyApplication.kt
+++ app/src/main/java/com/rorono/a22recycler/MyApplication.kt
@@ -1,11 +1,29 @@
 package com.rorono.a22recycler
 
 import android.app.Application
+import android.content.Context
 import com.rorono.a22recycler.di.AppComponent
 import com.rorono.a22recycler.di.DaggerAppComponent
 
-class MyApplication:Application() {
-    val appComponent:AppComponent by lazy {
+class MyApplication : Application() {
+
+    init {
+        instance = this
+    }
+
+    val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val context: Context = MyApplication.applicationContext()
+    }
+
+    companion object {
+        private var instance: MyApplication? = null
+        fun applicationContext(): Context {
+            return instance!!.applicationContext
+        }
     }
 }
